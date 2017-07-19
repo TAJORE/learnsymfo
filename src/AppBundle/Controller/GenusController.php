2b82b424df18941e11dcb69609869a17b7f66f61
TAJORE <jouanirenatot@gmail.com>
2017-07-19 16:53:58 +0200

    up

---
+++ src/AppBundle/Controller/GenusController.php
@@ -0,0 +1,14 @@
+<?php
+namespace AppBundle\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+class GenusController
+{
+    /**
+     * @Route("/genus/{genusName}")
+     */
+    public function showAction($genusName)
+    {
+        return new Response('The genus: '.$genusName);
+    }
+}
